945b7ff84e1c3167ec7fdd5c9bc8ee0447358e39
QuangQuyen <quangquyendad@gmail.com>
2023-05-09 00:39:46 +0700

    deploy

---
+++ components/auth/auth.ts
@@ -0,0 +1,10 @@
+// import App from 'next/app'
+// import React from 'react'
+
+// function auth(component:App) {
+//   return (
+//     <div>auth</div>
+//   )
+// }
+
+// export default auth
--- models/config.ts
+++ models/config.ts
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME_NEW,process.env.DB_USER_NEW, process.env.DB_PASS_NEW, {
   host: process.env.DB_HOST_NEW,
   dialect: 'mysql',
+  dialectModule: require('mysql2'),
   port: '3306',
   logging: false
 
