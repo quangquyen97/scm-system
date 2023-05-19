d5de279bef5c9bf9cf4d672c5a89337c81daf180
QuangQuyen <quangquyendad@gmail.com>
2023-05-19 11:09:30 +0700

    lÃastest

--- models/config.ts
+++ models/config.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(process.env.DB_NAME_NEW as string, process.env.DB_USER_NEW as string, process.env.DB_PASS_NEW as string, {
 
-  host: process.env.DB_HOST_NEW,
+  host: process.env.DB_HOST_NEW as string,
   dialect: 'mysql',
   dialectModule: require('mysql2'),
   port: '3306' as unknown as number,
