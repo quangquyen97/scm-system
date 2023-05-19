e7b18ccbccbc09ee87b46891dce997270fe47e41
QuangQuyen <quangquyendad@gmail.com>
2023-05-19 10:43:26 +0700

    fixdeploy

--- components/templates/user/user-info.tsx
+++ components/templates/user/user-info.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { decode } from "../../../lib/auth";
+import { decode } from "../../../middleware/auth";
 
 function AccountInfo() {
   const [account, setAccount] = useState({
